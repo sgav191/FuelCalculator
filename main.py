3fdf1b93cd8a7d85422f9c77550f2374b58fd42c
sgav191 <swgavin0@gmail.com>
2021-11-24 11:32:37 +0000

    Corrected some pylint errors

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+"""
+To add time delays in this project
+"""
 import time
-import random
-from decimal import Decimal, ROUND_UP, ROUND_DOWN, ROUND_HALF_UP
+from decimal import Decimal, ROUND_DOWN
 fuelregions = ["1: London","2: Northern Ireland","3: Wales","4: Scotland","5: South West","6: South East","7: East of England","8: East Midlands","9: West Midlands","10: Yorkshire and the Humber","11: North East","12: North West"]
 fuelprices = [[142,145],[140,142],[141,145],[141,145],[142,145],[142,146],[142,145],[141,145],[141,145],[141,145],[140,144],[141,145]]
 
@@ -9,8 +11,8 @@ time.sleep(1)
 print ("With this groundbreaking utility, you can calculate the approximate amount you'll have to pay for fuel in your area.")
 input("Press enter when you are ready to continue and see your options...")
 for i in fuelregions:
-	print (i)
-	time.sleep(0.25)
+    print (i)
+    time.sleep(0.25)
 userregionchoice = int(input ("Please type in the number of your region here: "))
 userregionchoice = userregionchoice-1
 print ("What type of fuel does your car use?")
@@ -27,4 +29,3 @@ userfuelprice = (userfuelrate*userfuelquantity)/100
 userfuelprice = Decimal(userfuelprice).quantize(Decimal('.01'), rounding=ROUND_DOWN)
 print (f"""Your fuel price is:
 £{userfuelprice}""")
-
