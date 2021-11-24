16450d4f56129c8ef9e2b94d4615bcb22b57b720
sgav191 <swgavin0@gmail.com>
2021-11-24 14:00:29 +0000

    Added support for Super Unleaded

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-"""
-To add time delays in this project
-"""
 import time
 from decimal import Decimal, ROUND_DOWN
-fuelregions = ["1: London","2: Northern Ireland","3: Wales","4: Scotland","5: South West","6: South East","7: East of England","8: East Midlands","9: West Midlands","10: Yorkshire and the Humber","11: North East","12: North West"]
-fuelprices = [[142,145],[140,142],[141,145],[141,145],[142,145],[142,146],[142,145],[141,145],[141,145],[141,145],[140,144],[141,145]]
+fuelregions = ["1: London", "2: Northern Ireland",  "3: Wales", "4: Scotland", "5: South West", "6: South East", "7: East of England", "8: East Midlands",  "9: West Midlands", "10: Yorkshire and the Humber", "11: North East", "12: North West"]
+fuelprices = [[142, 145, 154], [140, 142, 150], [141, 145, 150], [141, 145, 150], [142, 145, 151], [142, 146, 153], [142, 145, 152], [141, 145, 153], [141, 145, 152], [141, 145, 151], [140, 144, 152], [141, 145, 152]]
 
-print ("Hello and welcome to FUELCALC-2.0!")
+print("Hello and welcome to FUELCALC-2.0!")
 time.sleep(1)
-print ("With this groundbreaking utility, you can calculate the approximate amount you'll have to pay for fuel in your area.")
+print("With this groundbreaking utility,  you can calculate the approximate amount you'll have to pay for fuel in your area.")
 input("Press enter when you are ready to continue and see your options...")
 for i in fuelregions:
-    print (i)
+    print(i)
     time.sleep(0.25)
-userregionchoice = int(input ("Please type in the number of your region here: "))
+userregionchoice = int(input("Please type in the number of your region here: "))
 userregionchoice = userregionchoice-1
-print ("What type of fuel does your car use?")
-print ("""Choose from the list:
+print("What type of fuel does your car use?")
+print("""Choose from the list:
 1: Unleaded
 2: Diesel
 3: Super Unleaded
 """)
-userfuelchoice = int(input ("Please type your answer here: "))
+userfuelchoice = int(input("Please type your answer here: "))
 userfuelchoice = userfuelchoice-1
 userfuelrate = fuelprices[userregionchoice][userfuelchoice]
-userfuelquantity = int(input ("How much fuel do you want, in litres: "))
+userfuelquantity = int(input("How much fuel do you want,  in litres: "))
 userfuelprice = (userfuelrate*userfuelquantity)/100
-userfuelprice = Decimal(userfuelprice).quantize(Decimal('.01'), rounding=ROUND_DOWN)
-print (f"""Your fuel price is:
+userfuelprice = Decimal(userfuelprice).quantize(Decimal('.01'),  rounding=ROUND_DOWN)
+print(f"""Your fuel price is:
 £{userfuelprice}""")
